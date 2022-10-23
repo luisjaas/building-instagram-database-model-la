b1806ec1df036ceb63f3bf7cad9cbf7215c1c692
luisjaas <luisjaas98@gmail.com>
2022-10-23 05:04:32 +0000

    proyecto sql

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,72 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+#L 16-19 indicar al sql cual relacion es
+class Follow(Base):
+    __tablename__ = 'follow'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    follower = relationship('User', foreign_keys= [follower_id]) 
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', foreign_keys= [user_id])
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    email = Column(String(50), nullable=False, unique=True)
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False)
+
+
+class Post(Base):
+    __tablename__ = 'Post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    tipo_post = Column(String(5), nullable=False)
+    fecha = Column(DateTime(), nullable=False)
+    text = Column(String(50), nullable=False)
+
+
+
+class Like(Base):
+    __tablename__ = 'Like'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('user')
+    pos_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('post')
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('user')
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('post')
+    comentarios = Column(String(200), nullable=False)
+
+
+
+
+
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
 
     def to_dict(self):
         return {}
